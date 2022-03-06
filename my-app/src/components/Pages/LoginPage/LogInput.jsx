345bd5efa32b291ec057c3a98e8cfa347d5bbb59
bhaskarkrp <bhaskarkumar0022@gmail.com>
2022-03-06 15:01:57 +0530

    Finalizing Login

--- my-app/src/components/Pages/LoginPage/LogInput.jsx
+++ my-app/src/components/Pages/LoginPage/LogInput.jsx
@@ -4,14 +4,16 @@ import LandingPageContext from "../LandingPage/ContextLandingPage/ContextLanding
 import styles from "./loginput.module.css";
 import React, { useContext, useRef, useState } from "react";
 export const LogInput = () => {
-  
 
-  const {isLogin,setIsLogin}=useContext(LandingPageContext)
+
+  const { isLogin, setIsLogin } = useContext(LandingPageContext)
+  const [alert, setAlert] = React.useState(false);
+
   const logInUserName = useRef()
   const logInPassword = useRef()
   const navigate = useNavigate()
 
-  async function postMe(data){
+  async function postMe(data) {
 
     let res = await fetch(`https://masai-api-mocker.herokuapp.com/auth/login`, {
       method: "POST",
@@ -21,49 +23,53 @@ export const LogInput = () => {
     let dataSend = await res.json();
     return dataSend
   }
-  
-  const loginHandler = ()=>{
-    const login_data ={
-     username:logInUserName.current.value,
-     password:logInPassword.current.value
+
+  const loginHandler = () => {
+    const login_data = {
+      username: logInUserName.current.value,
+      password: logInPassword.current.value
     }
 
 
     console.log(login_data);
-    postMe(login_data).then((res)=>{
+    postMe(login_data).then((res) => {
       setIsLogin(true)
       console.log(res);
-    }).catch((err)=>{
-      alert("Signup First")
-      navigate("/signup") 
+    }).catch((err) => {
+      // alert("Signup First")
+      setAlert(true)
+      // navigate("/signup")
     })
   }
 
   return (
     <div className={styles.rightSection}>
       <div className={styles.EmailDiv}>
-        <input placeholder="User Name" ref={logInUserName } required />
+        <input placeholder="User Name" ref={logInUserName} required />
         <hr />
         <p>
           Login <a href="/">with an OTP</a>{" "}
         </p>
       </div>
       <div className={styles.PasswordDiv}>
-        <input type="password" placeholder="Password"  ref={logInPassword} required/>
+        <input type="password" placeholder="Password" ref={logInPassword} required />
         <hr />
         <p>
           <a href="/">Forgot your password?</a>{" "}
         </p>
       </div>
+      <div>
+        {alert && <p style={{ color: 'red', marginTop: "3rem" }}>User doesn't exist. Please <span style={{ cursor: "pointer" }} onClick={() => navigate("/signup")}>Signup</span>!</p>}
+      </div>
       <button className={styles.ContinueBtn} onClick={loginHandler} >Continue</button>
       <div className={styles.SignUpButtons}>
-      <p>Do not have an IKEA account? Create one now:</p>
-      <button className={styles.ContinueBtn} onClick={()=>{navigate("/signup") }} >I'm buying for my home</button>
-      <button className={styles.ContinueBtn}  onClick={()=>{navigate("/signup") }} >I'm buying for my company</button>
+        <p>Do not have an IKEA account? Create one now:</p>
+        <button className={styles.ContinueBtn} onClick={() => { navigate("/signup") }} >I'm buying for my home</button>
+        <button className={styles.ContinueBtn} onClick={() => { navigate("/signup") }} >I'm buying for my company</button>
 
       </div>
-    
 
-    </div>
+
+    </div >
   );
 };
